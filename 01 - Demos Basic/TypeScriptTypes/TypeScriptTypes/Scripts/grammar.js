f3faf4469a97647e31436187e19b8b76d909e039
Fabiano Alberto Nalin <fnalin@msn.com>
2016-06-06 22:59:00 -0300

    Adicionando Exemplo TypeScriptTypes - 01

---
+++ 01 - Demos Basic/TypeScriptTypes/TypeScriptTypes/Scripts/grammar.js
@@ -0,0 +1,21 @@
+var x = 1;
+var y;
+var firstName = 'Fabiano';
+var lastName = 'Nalin';
+var num1 = 100;
+var num2 = 20;
+/*
+//Sem js - não gera erra de compilação
+function addNumbers(n1, n2, n3) {
+    var result = n1 + n2 + n3;
+    alert('A soma é = ' + result);
+};
+addNumbers(num1, num2, 'teste');
+*/
+function addNumbers(n1, n2, n3) {
+    var result = n1 + n2 + n3;
+    alert('A soma é = ' + result);
+}
+;
+//addNumbers(num1, num2, 'teste');
+addNumbers(num1, num2, 19);
---
+++ 01 - Demos Basic/TypeScriptTypes/TypeScriptTypes/Scripts/grammar.ts
@@ -0,0 +1,27 @@
+﻿var x = 1;
+
+var y;
+
+var firstName = 'Fabiano';
+
+var lastName: string = 'Nalin';
+
+var num1 = 100;
+var num2: number = 20;
+
+/*
+//Sem js - não gera erra de compilação
+function addNumbers(n1, n2, n3) {
+    var result = n1 + n2 + n3;
+    alert('A soma é = ' + result);
+};
+addNumbers(num1, num2, 'teste');
+*/
+
+
+function addNumbers(n1:number, n2:number, n3:number) {
+    var result = n1 + n2 + n3;
+    alert('A soma é = ' + result);
+};
+//addNumbers(num1, num2, 'teste');
+addNumbers(num1, num2, 19);
